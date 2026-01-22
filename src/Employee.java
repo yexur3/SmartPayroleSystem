4604cc0402e7409b5e16a2ab375a35dda5b5668e
Illia <isapa@student.umb.sk>
2026-01-22 17:15:01 +0100

    commit

---
+++ src/Employee.java
@@ -0,0 +1,40 @@
+public abstract class Employee {
+    protected String name;
+    protected int id;
+    protected double baseSalary;
+
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public abstract double calculateSalary();
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public double getBaseSalary(){
+        return baseSalary;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setBaseSalary(double baseSalary) throws Vynimka {
+        if (baseSalary <= 0){
+            throw new Vynimka(baseSalary);
+        } else {
+            this.baseSalary = baseSalary;
+        }
+    }
+}
